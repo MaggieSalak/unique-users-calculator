444501a6f17745508f0224937c41090b3f615bb7
MaggieSalak <39058899+MaggieSalak@users.noreply.github.com>
2019-09-30 17:17:14 +0200

    Update Main.java

--- src/com/salak/maggie/Main.java
+++ src/com/salak/maggie/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Starting the consumer...");
-        DataConsumer consumer = new DataConsumer("data-challenge-topic");
+        DataConsumer consumer = new DataConsumer("unique-visits-topic");
         consumer.start();
     }
 }
